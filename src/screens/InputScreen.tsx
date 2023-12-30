309715bf990eaa3fca0d03ed70375600816686fe
kmamk328 <pinponpan328@gmail.com>
2023-12-30 12:39:28 +0900

    20231230

--- src/screens/InputScreen.tsx
+++ src/screens/InputScreen.tsx
@@ -1,15 +1,27 @@
+
 import React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const InputScreen = () => {
+  const navigation = useNavigation();
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          onPress={() => {/* 設定画面への遷移ロジック */}}
+          title="設定"
+        />
+      ),
+    });
+  }, [navigation]);
+
   // スコア入力に関するロジックはここに実装
 
   return (
     <SafeAreaView>
       <Text>{/* 日付の表示 */}</Text>
-      <TouchableOpacity onPress={() => {/* 設定画面への遷移ロジック */}}>
-        <Text>設定</Text>
-      </TouchableOpacity>
       {/* スコア入力テーブルの表示 */}
     </SafeAreaView>
   );
